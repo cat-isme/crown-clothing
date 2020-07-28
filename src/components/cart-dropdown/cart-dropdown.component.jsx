2b8db8f14bbbf6454a31e931641f32db9fadd3af
cat-isme <phuonganh1902.le@gmail.com>
2020-07-28 07:13:33 +0700

    shopping cart and update application with reselect

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import CustomButton from "../custom-button/custom-button.component";
-
+import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 import "./cart-dropdown.style.scss";
 
-const CartDropDown = () => (
+const CartDropDown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items"></div>
+    <div className="cart-items">
+      {cartItems.map((cartItem) => (
+        <CartItem key={cartItem.key} item={cartItem}></CartItem>
+      ))}
+    </div>
     <CustomButton>GO TO CHECK OUT</CustomButton>
   </div>
 );
 
-export default CartDropDown;
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
+});
+
+export default connect(mapStateToProps)(CartDropDown);
