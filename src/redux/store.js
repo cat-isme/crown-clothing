34e922b03734d1539b4ebd4072bbe130bf0cdfec
Chloe-Le97 <phuonganh1902.le@gmail.com>
2020-09-10 20:24:35 +0700

    birng data to redux store and create route in shop page

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
@@ -11,4 +12,6 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
