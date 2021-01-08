dfe28da9ac15c8f2b927f8fa7c665b4a53025a1a
Chloe-Le97 <phuonganhkitty365@gmail.com>
2021-01-08 18:08:46 +0200

    deploy to heroku

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
@@ -6,10 +6,11 @@ import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
-  applyMiddleware(...middlewares) + window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(...middlewares))
 );
 
 const persistor = persistStore(store);
